88efe39079fc9dfa9a1c169e5dbef154154b49f1
Jonas Gauffin <jonas.gauffin@coderr.io>
2023-04-12 14:39:01 +0200

    Fixed a compilation error

---
+++ src/Griffin.Data.Tests/ScaffolderTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+using Griffin.Data.Scaffolding.Mapper;
+
+namespace Griffin.Data.Tests;
+
+public class ScaffolderTest
+{
+    [Fact]
+    public async Task Test1()
+    {
+        var s = new MapperScaffolder();
+        await using var connection = new SqlConnection("Data Source=.;Initial Catalog=GriffinData;Integrated Security=True");
+        connection.Open();
+        await s.Generate(connection, @"C:\temp");
+    }
+}
