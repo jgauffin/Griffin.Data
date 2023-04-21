9888b1e002ee6bdfe659fd947f4972ba67828588
Jonas Gauffin <jonas.gauffin@coderr.io>
2023-04-21 21:41:35 +0200

    Made CRUD operations public.

--- src/Griffin.Data/Domain/Implementation/CrudOperations.cs
+++ src/Griffin.Data/Domain/Implementation/CrudOperations.cs
@@ -1,28 +1,45 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Griffin.Data.Mapper;
 
 namespace Griffin.Data.Domain.Implementation;
 
-internal class CrudOperations<TEntity> : ICrudOperations<TEntity>
+/// <summary>
+///     Performs CRUD for entities.
+/// </summary>
+/// <typeparam name="TEntity">Type of entity.</typeparam>
+/// <remarks>
+///     <para>
+///         Typically used as a base class for repositories.
+///     </para>
+/// </remarks>
+public class CrudOperations<TEntity> : ICrudOperations<TEntity>
 {
     private readonly Session _session;
 
+    /// <summary>
+    /// </summary>
+    /// <param name="session">Session sued to </param>
+    /// <exception cref="ArgumentNullException"></exception>
     public CrudOperations(Session session)
     {
-        _session = session;
+        _session = session ?? throw new ArgumentNullException(nameof(session));
     }
 
+    /// <inheritdoc />
     public async Task Create([DisallowNull] TEntity entity)
     {
         await _session.Insert(entity);
     }
 
+    /// <inheritdoc />
     public async Task Delete([DisallowNull] TEntity entity)
     {
         await _session.Delete(entity);
     }
 
+    /// <inheritdoc />
     public async Task Update([DisallowNull] TEntity entity)
     {
         await _session.Update(entity);
